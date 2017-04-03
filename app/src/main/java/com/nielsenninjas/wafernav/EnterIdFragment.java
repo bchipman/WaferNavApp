065d38c69b3bcff35e0e5aaf52c450ebf70a22af
Brian Chipman <namedtuple@gmail.com>
2017-04-02 17:33:19 -0700

    Separated top half of MainActivity into EnterIdFragment.  Using fragments will reduce overhead in transitioning to different screens as opposed to creating a new Activity for each screen.  Eventually bottom half will be removed, keeping it for debugging purposes for now.

---
+++ app/src/main/java/com/nielsenninjas/wafernav/EnterIdFragment.java
@@ -0,0 +1,186 @@
+package com.nielsenninjas.wafernav;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
+
+/**
+ A simple {@link Fragment} subclass.
+ Activities that contain this fragment must implement the
+ {@link EnterIdFragment.OnFragmentInteractionListener} interface
+ to handle interaction events.
+ Use the {@link EnterIdFragment#newInstance} factory method to
+ create an instance of this fragment.
+ */
+public class EnterIdFragment extends Fragment {
+
+    private static final String TAG = "EnterIdFragment";
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // UI elements
+    protected AutoCompleteTextView mAutoCompleteTextViewId;
+    protected ScrollView mScrollViewOutputLog;
+    protected TextView mTextViewOutputLog;
+
+    public EnterIdFragment() {
+        // Required empty public constructor
+    }
+
+    public static EnterIdFragment newInstance(String param1, String param2) {
+        EnterIdFragment fragment = new EnterIdFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_enter_id, container, false);
+
+        // Hide keyboard when (1) click non-EditText object, or (2) press enter in EditText object
+        setupHideKeyboardListeners(view);
+        Log.i(TAG, "onCreate()");
+
+        // Set the UI elements
+        mTextViewOutputLog = (TextView) getActivity().findViewById(R.id.textViewOutputLog);
+        mScrollViewOutputLog = (ScrollView) getActivity().findViewById(R.id.scrollViewOutputLog);
+
+        // AutoCompleteTextView for IDs
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.ids, android.R.layout.simple_dropdown_item_1line);
+        mAutoCompleteTextViewId = (AutoCompleteTextView) view.findViewById(R.id.autoCompleteTextViewId);
+        mAutoCompleteTextViewId.setAdapter(adapter);
+
+        // Focus publish button when start app
+        view.findViewById(R.id.buttonPublish).requestFocus();
+        hideKeyboard();
+
+
+        // Set button handlers
+        Button publishButton = (Button) view.findViewById(R.id.buttonPublish);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.publishButtonHandler(mAutoCompleteTextViewId);
+                }
+            }
+        });
+
+        Button readBarcodeButton = (Button) view.findViewById(R.id.buttonReadBarcode);
+        readBarcodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.readBarcodeButtonHandler(mAutoCompleteTextViewId);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (getActivity().getCurrentFocus() != null) {
+            inm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+        }
+        else {
+            Log.w(TAG, "I WOULD HAVE CRASHED BECAUSE NOTHING IS FOCUSED!!");
+        }
+        getActivity().findViewById(R.id.parent).clearFocus();
+    }
+
+    private void setupHideKeyboardListeners(final View view) {
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideKeyboard();
+                    return false;
+                }
+            });
+        }
+        // Set up editor listener to hide keyboard when press enter in TextEdit object
+        else {
+            ((EditText) view).setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                        hideKeyboard();
+                        // if this is mEditTextId, also trigger the 'Publish' button when press enter
+                        if (view == mAutoCompleteTextViewId) {
+                            mListener.publishButtonHandler(mAutoCompleteTextViewId);
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+
+        // If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupHideKeyboardListeners(innerView);
+            }
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        }
+        else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     This interface must be implemented by activities that contain this
+     fragment to allow an interaction in this fragment to be communicated
+     to the activity and potentially other fragments contained in that
+     activity.
+     See the Android Training lesson <a href=
+     "http://developer.android.com/training/basics/fragments/communicating.html"
+     >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void publishButtonHandler(View view);
+        void readBarcodeButtonHandler(View view);
+    }
+
+}
