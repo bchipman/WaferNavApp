fd126ac3dbfeee0a54081fe0926e3ae5bcdcc7bd
pilillani <lorenia.jimenez@gmail.com>
2017-04-10 12:31:25 -0700

    Confirm delivery screen

---
+++ app/src/main/java/com/nielsenninjas/wafernav/delivering_to.java
@@ -0,0 +1,13 @@
+package com.nielsenninjas.wafernav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class delivering_to extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivering_to);
+    }
+}
