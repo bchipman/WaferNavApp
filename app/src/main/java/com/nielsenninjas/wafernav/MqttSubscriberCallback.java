80a5a748b893baa4e986ab4f5fcc49ae8cb8b59c
Brian Chipman <namedtuple@gmail.com>
2017-04-20 22:38:19 -0700

    Further utilization of Field enum.

--- app/src/main/java/com/nielsenninjas/wafernav/MqttSubscriberCallback.java
+++ app/src/main/java/com/nielsenninjas/wafernav/MqttSubscriberCallback.java
@@ -3,6 +3,7 @@ package com.nielsenninjas.wafernav;
 import android.app.Fragment;
 import android.util.Log;
 import com.nielsenninjas.wafernav.enums.Directive;
+import com.nielsenninjas.wafernav.enums.Fields;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -10,7 +11,6 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.IOException;
-import java.util.IllegalFormatException;
 import java.util.Map;
 
 /**
@@ -53,10 +53,10 @@ public class MqttSubscriberCallback implements MqttCallback {
             return;
         }
 
-        Fragment fragment = null;
+        Fragment fragment;
         Directive directive;
         try {
-            directive = Directive.valueOf(jsonMap.get("directive"));
+            directive = Directive.valueOf(jsonMap.get(Fields.DIRECTIVE.field()));
         }
         catch (Exception e) {
             directive = Directive.NULL;
@@ -70,15 +70,15 @@ public class MqttSubscriberCallback implements MqttCallback {
 
             case GET_NEW_BLU_RETURN:
                 Log.i(TAG, Directive.GET_NEW_BLU_RETURN.toString());
-                id = jsonMap.get("bluId");
-                location = jsonMap.get("bluInfo");
+                id = jsonMap.get(Fields.BLU_ID.field());
+                location = jsonMap.get(Fields.BLU_INFO.field());
                 fragment = AssignHandlerFragment.newInstance(mMainActivity.getCurrentOperation(), id, location);
                 break;
 
             case COMPLETE_NEW_BLU_RETURN:
                 Log.i(TAG, Directive.COMPLETE_NEW_BLU_RETURN.toString());
-                confirmed = jsonMap.get("confirm");
-                if (!confirmed.equals("true")) {
+                confirmed = jsonMap.get(Fields.CONFIRM.field());
+                if (!confirmed.equals(Fields.TRUE.field())) {
                     Log.e(TAG, "Confirmed was not true.");
                     return;
                 }
@@ -87,15 +87,15 @@ public class MqttSubscriberCallback implements MqttCallback {
 
             case GET_NEW_SLT_RETURN:
                 Log.i(TAG, Directive.GET_NEW_SLT_RETURN.toString());
-                id = jsonMap.get("sltId");
-                location = jsonMap.get("sltInfo");
+                id = jsonMap.get(Fields.SLT_ID.field());
+                location = jsonMap.get(Fields.SLT_INFO.field());
                 fragment = DeliveringToFragment.newInstance(mMainActivity.getCurrentOperation(), id, location);
                 break;
 
             case COMPLETE_NEW_SLT_RETURN:
                 Log.i(TAG, Directive.COMPLETE_NEW_SLT_RETURN.toString());
-                confirmed = jsonMap.get("confirm");
-                if (!confirmed.equals("true")) {
+                confirmed = jsonMap.get(Fields.CONFIRM.field());
+                if (!confirmed.equals(Fields.TRUE.field())) {
                     Log.e(TAG, "Confirmed was not true.");
                     return;
                 }
--- app/src/main/java/com/nielsenninjas/wafernav/enums/Fields.java
+++ app/src/main/java/com/nielsenninjas/wafernav/enums/Fields.java
@@ -8,10 +8,12 @@ public enum Fields {
     DIRECTIVE("directive"),
     LOT_ID("lotId"),
     SLT_ID("sltId"),
+    SLT_INFO("sltInfo"),
     BIB_IDS("bibIds"),
     BLU_ID("bluId"),
     BLU_INFO("bluInfo"),
-    CONFIRM("confirm");
+    CONFIRM("confirm"),
+    TRUE("true");
 
     private String field;
 
